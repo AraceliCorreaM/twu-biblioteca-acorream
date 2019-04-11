35cf416f7512ef4a5683d0b99ce3af0722c65feb
Araceli Correa <araceli.correa@thoughtworks.com>
2019-04-11 15:26:08 -0400

    [Araceli][#1.1] View a welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,29 @@
 package com.twu.biblioteca;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
 public class BibliotecaApp {
+    private static BibliotecaApp biblioteca;
+    private final PrintStream out;
+    private BufferedReader in;
+
+    public BibliotecaApp(BufferedReader in, PrintStream out) {
+        this.out = out;
+        this.in = in;
+    }
+
+    public void start() {
+        this.getWelcomeMessage();
+    }
+
+    public void getWelcomeMessage() {
+        //System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!.");
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!.");
     }
 }
---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,37 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.contains;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+public class BibliotecaAppTest {
+    private BibliotecaApp bibliotecaApp;
+    private BufferedReader in;
+    private PrintStream out;
+
+    @Before
+    public void setUp(){
+
+        in = mock(BufferedReader.class);
+        out = mock(PrintStream.class);
+        bibliotecaApp = new BibliotecaApp(in,out);
+    }
+
+    @Test
+    public void shouldPrintWelcomeMessageWhenStart() throws IOException{
+        when(in.readLine()).thenReturn("0");
+        bibliotecaApp.start();
+        //verify(out).println(contains("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!."));
+    }
+}
