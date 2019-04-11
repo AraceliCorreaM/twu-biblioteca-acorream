ecd91524a29ab85ad92d2c05ffb08eb79d6c5ffa
Araceli Correa <araceli.correa@thoughtworks.com>
2019-04-11 17:07:40 -0400

    [Araceli][#1.4] View a main menu of options

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,52 +1,93 @@
 package com.twu.biblioteca;
 
-import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BibliotecaApp {
-    private ArrayList<Book> bookList;
-    //private final PrintStream out;
-    //private BufferedReader in;
 
-    public BibliotecaApp(ArrayList<Book> listOfBooks){//, BufferedReader in, PrintStream out) {
+    private ArrayList<Book> bookList;
+    private final PrintStream out;
+    private Scanner scanner;
+    private Boolean inputValid;
+
+    public BibliotecaApp(ArrayList<Book> listOfBooks, InputStream in, PrintStream out) {
         this.bookList = listOfBooks;
-        //this.out = out;
-        //this.in = in;
+        this.out = out;
+        this.scanner = new Scanner(in);
+        this.inputValid = true;
     }
 
-    public void start() {
-        this.getWelcomeMessage();
-        if(bookList.isEmpty()){
-            System.out.println("There is not Books Available.");
-        } else {
-            System.out.println("Books Available:");
-            this.listBooks(bookList);
+    private void applySelectedMenuOption(Integer input) {
+        switch (input) {
+            case 0:
+                this.quit();
+                break;
+            case 1:
+                this.listBooks(this.bookList);
+                break;
+            default:
+                this.out.println("Select a valid option!");
         }
     }
 
-    public void getWelcomeMessage() {
-        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!.");
+    private Integer getUserInput() {
+        Integer input = 0;
+        try {
+            this.out.print("\nMenu selection: \n");
+            input = this.scanner.nextInt();
+        } catch (Exception e) {
+
+        }
+        return input;
     }
 
-    public void listBooks(ArrayList<Book> bookList) {
+    public void respondToUserInput() {
+        while (this.inputValid) {
+            applySelectedMenuOption(getUserInput());
+        }
+    }
+
+    public void start(){
+        this.getWelcomeMessage();
+    }
+
+    public void quit(){
+        this.out.println("See you later");
+        this.inputValid = false;
+    }
+
+    public void getWelcomeMessage(){
+        this.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!.");
+        this.showMenuOptions();
+    }
+
+    public void showMenuOptions() {
+        this.out.println("Menu Options:");
+        this.out.println("Enter [1] to show all books");
+        this.out.println("Enter [0] to show quit the system");
+        this.respondToUserInput();
+    }
+
+    public void listBooks(ArrayList<Book> bookList){
         String output = "";
-        for (com.twu.biblioteca.Book book : bookList) {
+        for (Book book : bookList){
             output += book.getBookInfo() + "\n";
         }
-        System.out.print(output);
+        this.out.print(output);
     }
 
     public static void main(String[] args) {
         ArrayList<Book> bookList = new ArrayList();
-
         bookList.add(new Book("Inferno", "Dan Brown", 2014));
         bookList.add(new Book("You'll Grow Out Of It", "Jessi Klein", 2016));
         bookList.add(new Book("You Are A Badass: How to Stop Doubting Your Greatness and Start Living an Awesome Life", "Jen Sincero", 2013));
         bookList.add(new Book("Learn Python 3 The Hard Way: A Very Simple", "Zed A. Shaw", 2017));
 
-        BibliotecaApp biblioteca = new BibliotecaApp(bookList);
+        BibliotecaApp biblioteca = new BibliotecaApp(bookList, System.in, new PrintStream(System.out));
 
         biblioteca.start();
     }
+
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -3,35 +3,35 @@ package com.twu.biblioteca;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.contains;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 
 public class BibliotecaAppTest {
     private BibliotecaApp bibliotecaApp;
-    //private BufferedReader in;
-    //private PrintStream out;
+    private ArrayList<Book> bookList;
+    private Book Inferno;
+    private InputStream in;
+    private PrintStream out;
 
     @Before
     public void setUp(){
-
-        //in = mock(BufferedReader.class);
-        //out = mock(PrintStream.class);
-        //bibliotecaApp = new BibliotecaApp(in,out);
+        bookList = new ArrayList();
+        in = mock(InputStream.class);
+        out = mock(PrintStream.class);
+        Inferno = mock(Book.class);
+        bibliotecaApp = new BibliotecaApp(bookList,in,out);
     }
 
     @Test
-    public void shouldPrintWelcomeMessageWhenStart() throws IOException{
-        //when(in.readLine()).thenReturn("0");
-        //bibliotecaApp.start();
-        //verify(out).println(contains("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!."));
+    public void shouldPrintWelcomeMessageWhenStart(){
+        bibliotecaApp.start();
+        verify(out).println(contains("Welcome"));
     }
 }
